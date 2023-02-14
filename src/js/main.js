1aaee4f7877443486a4384c8ef25137a87e6bee7
iaZdelal <115097271+iaZdelal@users.noreply.github.com>
2023-02-15 01:07:43 +0600

    3.0

--- src/js/main.js
+++ src/js/main.js
@@ -32,3 +32,27 @@ document.querySelectorAll('.accordion').forEach((el) => {
         }
     });
 });
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      
+    },
+  });
