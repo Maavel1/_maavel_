7ccf7d8f009390901b718c768aa4e1c972db5377
iaZdelal <115097271+iaZdelal@users.noreply.github.com>
2023-02-15 21:48:12 +0600

    23

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
-
-
+'use strict';
 
 let header__burger = document.querySelector('.header-burger');
 let header_menu = document.querySelector('.header-menu');
@@ -18,8 +17,23 @@ header__list.onclick = function () {
 }
 
 
+const addEvenOneElements = function (elements, eventType, callback) {
+    for (let i = 0 , len = elements.length; i < len; i++ ) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
 
+const navToggLers = document.querySelectorAll('[data-nav-toggler]');
 
+const navToggleBtn = document.querySelector('[data-nav-toggle-btn]');
+
+const navBar = document.querySelector('[data-navbar]');
+
+const toggleNavbar = function() {
+    navBar.classList.toggle('active');
+    navToggleBtn.classList.toggle('active');
+    document.body.classList.toggle('nav-acitve');
+}
 
 addEvenOneElements(navToggLers,'click', toggleNavbar);
 
